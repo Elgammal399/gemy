7ed9884c5de18a04b5b94fe8e84e2da252be6075
Elgammal399 <133472333+Elgammal399@users.noreply.github.com>
2024-07-14 19:55:15 +0300

    Add files via upload

---
+++ j.s/indx.js
@@ -0,0 +1,35 @@
+
+
+
+
+// // Q2
+// var num = +prompt();
+
+// if(num % 3 == 0 && num % 4 == 0){
+// 	console.log("yes");
+// }else{
+// 	console.log("no");
+// }
+
+// Q3
+// var num1 = +prompt("enter num1");
+// var num2 = +prompt("enter num2");
+
+// if(num1 > num2){
+// 	console.log(num1);
+// }else{
+// 	console.log(num2);
+// }
+
+
+var num1 = +prompt("ENTER THE NIMBER")
+if(num1>0){
+	console.log("positive")
+	
+}
+else{
+	console.log("negative")
+}
+
+
+
